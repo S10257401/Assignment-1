d5edad48e71577de421c32a6c90db4e6ed430941
Xavier Lian <S10257401@connect.np.edu.sg>
2023-12-04 21:28:37 +0800

    Added a filter to the shop to make shopping easy

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-let cartOpen = false;
+function filterItems() {
+    const categoryFilter = document.getElementById('categoryFilter');
+    const selectedCategory = categoryFilter.value;
+    
+    const items = document.querySelectorAll('.flex-item');
 
-function navigateToCart() {
-    // Change 'cart.html' to the desired HTML file
-    window.location.href = 'cart.html';
-}
-
-function toggleCart() {
-    console.log("Cart clicked!");
-    const cartIcon = document.querySelector('.cart i');
-    cartOpen = !cartOpen;
-
-    if (cartOpen) {
-        // Add a visual indication when the cart is open (change color to red, for example)
-        cartIcon.style.color = 'red';
-        // Implement additional logic to show/hide the actual cart content
-    } else {
-        // Reset the visual indication when the cart is closed
-        cartIcon.style.color = 'white';
-        // Implement additional logic to show/hide the actual cart content
-    }
-}
-
-function addToCart(productId, productName, productPrice) {
-    // Implement logic to add items to the cart (you can use local storage, etc.)
-    console.log(`Added to cart: ${productName} - $${productPrice}`);
-    // Implement additional logic for the cart, e.g., update cart count, etc.
+    items.forEach(item => {
+        const category = item.dataset.category.toLowerCase();
+        
+        if (selectedCategory === 'all' || category === selectedCategory) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
 }
